2c857539315c40a79d3b2fce5bb807dcb50c0b25
thekevinchi <thekevinchi@gmail.com>
2016-12-09 23:31:29 +0000

    abstracted new report

---
+++ SaveTheChildren/app/src/main/java/com/cmamreport/savethechildren/newReport.java
@@ -0,0 +1,19 @@
+package com.cmamreport.savethechildren;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class newReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_report);
+    }
+
+
+    public void openDischarge(View view) {
+
+    }
+}
