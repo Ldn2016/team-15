13b5a1b8f52c711d11e3f229b34c826f367cf9a2
thekevinchi <thekevinchi@gmail.com>
2016-12-09 23:24:08 +0000

    filled first page of new report

---
+++ SaveTheChildren/app/src/main/java/com/cmamreport/savethechildren/NewAdmissions.java
@@ -0,0 +1,28 @@
+package com.cmamreport.savethechildren;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class NewAdmissions extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_admissions, container, false);
+
+        return view;
+    }
+
+    public void openDischarge(View view) {
+
+    }
+
+}
